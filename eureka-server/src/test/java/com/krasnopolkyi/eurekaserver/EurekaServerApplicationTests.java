04735a8da028d3ad7296d99bd83526b332b00ff9
MaxKrasnoloskyi <makskrasnopolskyi@gmail.com>
2024-12-11 12:16:22 +0200

    first commit

---
+++ eureka-server/src/test/java/com/krasnopolkyi/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krasnopolkyi.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
